b76798119a4606985488b9d86fa642d3061aa18b
evaponce2javi <163205482+evaponce2javi@users.noreply.github.com>
2024-04-25 02:51:14 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -121,7 +121,7 @@ Pair *firstMap(HashMap *map) {
 }
 
 Pair *nextMap(HashMap *map) {
-  for (long i = 0; i < map->capacity + 1; i++) {
+  for (long i = map->current + 1; i < map->capacity; i++) {
     if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
       map->current = i;
       return map->buckets[i];
