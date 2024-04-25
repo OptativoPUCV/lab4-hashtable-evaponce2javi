00c74407da7d4b16e5558f1251ad0b9105463417
evaponce2javi <163205482+evaponce2javi@users.noreply.github.com>
2024-04-25 02:29:06 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -95,6 +95,7 @@ void eraseMap(HashMap *map, char *key) {
 
 Pair *searchMap(HashMap *map, char *key) {
   long pos = hash(key, map->capacity);
+  long aux;
   if (map->buckets[pos]->key == key) {
     return map->buckets[pos];
   }
@@ -105,7 +106,7 @@ Pair *searchMap(HashMap *map, char *key) {
     }
     pos = (pos + 1) % map->capacity;
   }
-  return NULL;
+  return map->buckets[pos];
 }
 
 Pair *firstMap(HashMap *map) { return NULL; }
