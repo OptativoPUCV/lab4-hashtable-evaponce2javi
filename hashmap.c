3741cc8a858e290c8437567f3a53c77138c4354c
evaponce2javi <163205482+evaponce2javi@users.noreply.github.com>
2024-04-25 02:59:32 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -61,7 +61,7 @@ void insertMap(HashMap *map, char *key, void *value) {
 }
 
 void enlarge(HashMap *map) {
-  enlarge_called = 1; // no borrar (testing purposes)
+  // enlarge_called = 1; // no borrar (testing purposes)
   if (map == NULL)
     return;
   int old_c = map->capacity;
